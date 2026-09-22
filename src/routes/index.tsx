0bea61af9f4d9f2c164423b5de3ff6b8928ba10a
spookypetey <peterjm.contact@gmail.com>
2026-09-22 01:04:55 -0400

    Fix homepage logo import

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { createFileRoute } from "@tanstack/react-router";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { Play } from "lucide-react";
